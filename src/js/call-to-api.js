3325d524544228424310643d0a7d9b44035f5713
Rubenprada <rubenprada89@outlook.com>
2022-11-12 14:45:07 +0100

    FINISHED POKEAPI

--- src/js/call-to-api.js
+++ src/js/call-to-api.js
@@ -5,12 +5,12 @@ import { createPokemon, progressBars} from './print-pokemon.js'
 
 
 const fetchPokemons = async(offset = 1, limit = 8) => {
-  const pokemonRequests = [];
+  const allPokemon = [];
   for (let i = offset; i <= offset + limit; i++) {
       const promisePokemon = fetch('https://pokeapi.co/api/v2/pokemon/' + i).then(res => res.json());
-      pokemonRequests.push(promisePokemon);  
+      allPokemon.push(promisePokemon);  
   }
-  Promise.all(pokemonRequests).then(results => {
+  Promise.all(allPokemon).then(results => {
       results.forEach((pokemon, index) => {
           createPokemon(pokemon, index + 1);
       });
