c24186713bee423a0a79ae8556248f6a44519272
Rubenprada <rubenprada89@outlook.com>
2022-11-12 13:11:31 +0100

    FEAT SOLVED SEARCHER AND POKEMON ORDER

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 
+
+
+
+
 const pokemonContainer = document.querySelector(".pokemon-container");
 const previous = document.querySelector("#previous");
 const next = document.querySelector("#next");
@@ -150,6 +154,7 @@ function removeChildNodes(parent) {
 fetchPokemons();
 
 
+
 //Creo una variable para poder coger el formulario
 let pokeForm = document.getElementById('searchPokemon');
 //creo una función para extraer pokemon de la api para el buscador
@@ -161,36 +166,39 @@ function searchPokemon(pokemon) {
     });
 }
 //creo un evento con ese formulario
-pokeForm.addEventListener('submit', e => {
-  e.preventDefault();
-  //creo una variable para coger el valor del input
-  let searchPokemons = document.getElementById('pokemon').value.toLowerCase();
-  //le digo que si es true me lo muestre
-  searchPokemon(searchPokemons, true);
-  //que si el valor es true me quite el contenedor de los pokemon y me muestre el que busco
-  //y le paso la funcion de remove para eliminar a el hijo de pokemonContainer 
-  if (searchPokemons) {
-    removeChildNodes(pokemonContainer);
-    searchPokemon()
-  //si no meto valor me salte una alerta
-  } else {
-    alert('you have to put a value');
-    searchPokemon()
-  };
+const searcherForm = pokeForm.addEventListener('submit', e => {
+    e.preventDefault();
+    //creo una variable para coger el valor del input
+    let searchPokemons = document.getElementById('pokemon').value.toLowerCase();
+    //le digo que si es true me lo muestre
+    searchPokemon(searchPokemons, true);
+    //que si el valor es true me quite el contenedor de los pokemon y me muestre el que busco
+    //y le paso la funcion de remove para eliminar a el hijo de pokemonContainer 
+    if (searchPokemons) {
+      removeChildNodes(pokemonContainer);
+      searchPokemon()
+    //si no meto valor me salte una alerta
+    } else {
+      alert('you have to put a value');
+      searchPokemon()
+    };
+    
+    const main = document.querySelector('.main');
+    const div$$ = document.createElement('div');
+    const button = document.createElement('button');
+    button.setAttribute('type', 'reset')
+    button.textContent = 'Return';
+    div$$.appendChild(button);
+    const nav = document.getElementById('.pagination');
+    main.appendChild(div$$)
+    button.addEventListener('click', e =>{
+      removeChildNodes(pokemonContainer);
+      removeChildNodes(div$$);
+      fetchPokemons();
+    })
+  });
 
-  const main = document.querySelector('.main');
-  const div$$ = document.createElement('div');
-  const button = document.createElement('button');
-  button.setAttribute('name', 'remove')
-  button.textContent = 'Return';
-  div$$.appendChild(button);
-  const nav = document.getElementById('.pagination');
-  main.appendChild(div$$)
-  button.addEventListener('click', e =>{
-    removeChildNodes(pokemonContainer);
-    removeChildNodes(div$$);
-    fetchPokemons();
-  })
-});
 
-//export {fetchPokemon, fetchPokemons, previus, next, createPokemon, progressBars, pokeForm, pokeForm}
+
+
+
---
+++ src/js/searcher-pokemon.js
@@ -0,0 +1,47 @@
+'use strict';
+
+//Creo una variable para poder coger el formulario
+let pokeForm = document.getElementById('searchPokemon');
+//creo una función para extraer pokemon de la api para el buscador
+function searchPokemon(pokemon) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+    .then((res) => res.json())
+    .then((data) => {
+      createPokemon(data);
+    });
+}
+//creo un evento con ese formulario
+const searcherForm = pokeForm.addEventListener('submit', e => {
+    e.preventDefault();
+    //creo una variable para coger el valor del input
+    let searchPokemons = document.getElementById('pokemon').value.toLowerCase();
+    //le digo que si es true me lo muestre
+    searchPokemon(searchPokemons, true);
+    //que si el valor es true me quite el contenedor de los pokemon y me muestre el que busco
+    //y le paso la funcion de remove para eliminar a el hijo de pokemonContainer 
+    if (searchPokemons) {
+      removeChildNodes(pokemonContainer);
+      searchPokemon()
+    //si no meto valor me salte una alerta
+    } else {
+      alert('you have to put a value');
+      searchPokemon()
+    };
+  
+    const main = document.querySelector('.main');
+    const div$$ = document.createElement('div');
+    const button = document.createElement('button');
+    button.setAttribute('name', 'remove')
+    button.textContent = 'Return';
+    div$$.appendChild(button);
+    const nav = document.getElementById('.pagination');
+    main.appendChild(div$$)
+    button.addEventListener('click', e =>{
+      removeChildNodes(pokemonContainer);
+      removeChildNodes(div$$);
+      fetchPokemons();
+    })
+  });
+
+
+export {searcherForm};
